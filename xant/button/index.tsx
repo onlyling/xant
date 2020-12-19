137fed7e7de20fce01b39a3fb2157a1e76d558cf
LING_ZI_QING <onlylingstudio@gmail.com>
2020-12-19 18:52:56 +0800

    feat: 🎸 button
    按钮组件

---
+++ xant/button/index.tsx
@@ -0,0 +1,142 @@
+import React, { useRef, useEffect } from 'react';
+import {
+  Text,
+  TouchableHighlight,
+  Animated,
+  Easing,
+  GestureResponderEvent,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/AntDesign';
+
+import helpers from '../helpers';
+import { ButtonProps } from './interface';
+import { createStyles } from './style';
+import { Theme } from '../theme';
+
+/**
+ * 按钮
+ */
+const Button: React.FC<ButtonProps> = ({
+  children,
+  style,
+  text,
+  textStyle,
+  type = 'default',
+  size = 'normal',
+  plain = false,
+  hairline = false,
+  disabled = false,
+  loading = false,
+  loadingText,
+  square = false,
+  round = false,
+  icon,
+  iconStyle,
+  color,
+  onPress,
+  ...otherProps
+}) => {
+  const LoadingRef = useRef(new Animated.Value(0));
+  const { themeVar } = Theme.useContainer();
+
+  const Styles = createStyles(themeVar, {
+    size,
+    color,
+    type,
+    plain,
+    hairline,
+    square,
+    round,
+    disabled,
+  });
+
+  /**
+   * 点击按钮回调
+   * @param e event 回调事件
+   */
+  const onPressTouchable = (e: GestureResponderEvent) => {
+    if (!disabled) {
+      onPress && onPress(e);
+    }
+  };
+
+  // loading 状态
+  useEffect(() => {
+    const playerAnimated = Animated.timing(LoadingRef.current, {
+      toValue: 1,
+      duration: 1500,
+      easing: Easing.out(Easing.linear),
+      useNativeDriver: true,
+    });
+
+    let stop = false;
+
+    if (loading) {
+      const start = () => {
+        if (stop) {
+          return;
+        }
+
+        LoadingRef.current.setValue(0);
+
+        playerAnimated.start(() => {
+          start();
+        });
+      };
+
+      start();
+    }
+
+    return () => {
+      stop = true;
+      playerAnimated.stop();
+    };
+  }, [loading]);
+
+  const wrapperStyles = [Styles.wrapper, style];
+  const textStyles = [Styles.text, textStyle];
+  const iconStyles = [Styles.icon, iconStyle];
+
+  return (
+    <TouchableHighlight
+      underlayColor={helpers.hex2rgba(
+        (Styles.wrapper.backgroundColor === themeVar.white
+          ? themeVar.button_plain_underlay_color
+          : Styles.wrapper.backgroundColor) as string,
+      )}
+      onPress={onPressTouchable}
+      style={wrapperStyles}
+      {...otherProps}
+    >
+      {loading ? (
+        <>
+          <Animated.Text
+            style={[
+              iconStyles,
+              {
+                transform: [
+                  {
+                    rotateZ: LoadingRef.current.interpolate({
+                      inputRange: [0, 1],
+                      outputRange: ['0deg', '360deg'],
+                    }),
+                  },
+                ],
+              } as any,
+            ]}
+          >
+            <Icon name="loading1" />
+          </Animated.Text>
+          {loadingText ? <Text style={textStyles}>{loadingText}</Text> : null}
+        </>
+      ) : (
+        <>
+          {icon ? <Text style={iconStyles}>{icon}</Text> : null}
+          {text ? <Text style={textStyles}>{text}</Text> : children}
+        </>
+      )}
+    </TouchableHighlight>
+  );
+};
+
+export default Button;
