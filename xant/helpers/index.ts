af644acca7a73202ed013236799b571b84e7b0a8
LING_ZI_QING <onlylingstudio@gmail.com>
2020-12-28 23:39:53 +0800

    test: 💍 tests
    coverage

--- xant/helpers/index.ts
+++ xant/helpers/index.ts
@@ -1,9 +1,9 @@
-import normalizeText from './normalize-text';
-import normalizeSize from './normalize-size';
+// import normalizeText from './normalize-text';
+// import normalizeSize from './normalize-size';
 import hex2rgba from './hex2rgba';
 
 export default {
-  normalizeText,
-  normalizeSize,
+  // normalizeText,
+  // normalizeSize,
   hex2rgba,
 };
