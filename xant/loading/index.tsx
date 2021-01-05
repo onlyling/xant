f9f45a861822b8c404711f8c66e054c1a5656f5a
LING_ZI_QING <onlylingstudio@gmail.com>
2021-01-05 23:43:02 +0800

    feat: 🎸 loading
    Circular\Spinner svg

--- xant/loading/index.tsx
+++ xant/loading/index.tsx
@@ -1,9 +1,10 @@
-import React, { useRef, useCallback, useEffect, memo } from 'react';
-import { View, Text, Animated, Easing } from 'react-native';
-import Icon from 'react-native-vector-icons/AntDesign';
+import React, { memo } from 'react';
+import { View, Text } from 'react-native';
 
 import { LoadingProps } from './interface';
 import { createStyles } from './style';
+import Circular from './circular';
+import Spinner from './spinner';
 import { Theme } from '../theme';
 
 /**
@@ -21,59 +22,6 @@ const Loading: React.FC<LoadingProps> = ({
   const { themeVar } = Theme.useContainer();
   const Styles = createStyles(themeVar, { size, color, textSize, vertical });
 
-  const circleAnim = useRef(new Animated.Value(0)).current;
-  const circleInstance = useRef<Animated.CompositeAnimation | null>(null);
-  const stopShow = useCallback(() => {
-    if (circleInstance.current) {
-      circleInstance.current.stop();
-      circleInstance.current = null;
-    }
-  }, [circleInstance]);
-
-  useEffect(() => {
-    let stop = false;
-
-    const start = () => {
-      if (stop) {
-        return;
-      }
-
-      circleInstance.current = Animated.timing(circleAnim, {
-        toValue: 1,
-        duration: themeVar.loading_spinner_animation_duration * 1000,
-        easing: Easing.out(Easing.linear),
-        useNativeDriver: true,
-      });
-
-      circleAnim.setValue(0);
-
-      circleInstance.current.start(() => {
-        start();
-      });
-    };
-
-    start();
-
-    return () => {
-      stop = true;
-      stopShow();
-    };
-  }, [circleAnim, stopShow, themeVar.loading_spinner_animation_duration]);
-
-  const iconStyles = [
-    Styles.icon,
-    {
-      transform: [
-        {
-          rotateZ: circleAnim.interpolate({
-            inputRange: [0, 1],
-            outputRange: ['0deg', '360deg'],
-          }),
-        },
-      ],
-    },
-  ];
-
   const TextJSX = children ? (
     React.isValidElement(children) ? (
       children
@@ -84,12 +32,11 @@ const Loading: React.FC<LoadingProps> = ({
 
   return (
     <View style={Styles.loading}>
-      <Animated.View style={iconStyles}>
-        <Icon
-          name={type === 'circular' ? 'loading1' : 'sync'}
-          style={Styles.iconText}
-        />
-      </Animated.View>
+      {type === 'circular' ? (
+        <Circular size={size} color={color} />
+      ) : (
+        <Spinner size={size} color={color} />
+      )}
       {TextJSX}
     </View>
   );
--- xant/theme/default-var.ts
+++ xant/theme/default-var.ts
@@ -19,14 +19,14 @@ export const primary: string = blue;
 // Component Colors
 export const text_color = gray_8;
 export const active_color = gray_2;
-export const active_opacity = 0.7;
-export const disabled_opacity = 0.5;
+export const active_opacity: number = 0.7;
+export const disabled_opacity: number = 0.5;
 export const background_color = gray_1;
 export const background_color_light: string = '#fafafa';
 export const text_link_color: string = '#576b95';
 
 // Padding
-export const padding_base = 4;
+export const padding_base: number = 4;
 export const padding_xs = padding_base * 2;
 export const padding_sm = padding_base * 3;
 export const padding_md = padding_base * 4;
@@ -34,15 +34,15 @@ export const padding_lg = padding_base * 6;
 export const padding_xl = padding_base * 8;
 
 // Font
-export const font_size_xs = 10;
-export const font_size_sm = 12;
-export const font_size_md = 14;
-export const font_size_lg = 16;
-export const font_weight_bold = 500;
-export const line_height_xs = 14;
-export const line_height_sm = 18;
-export const line_height_md = 20;
-export const line_height_lg = 22;
+export const font_size_xs: number = 10;
+export const font_size_sm: number = 12;
+export const font_size_md: number = 14;
+export const font_size_lg: number = 16;
+export const font_weight_bold: number = 500;
+export const line_height_xs: number = 14;
+export const line_height_sm: number = 18;
+export const line_height_md: number = 20;
+export const line_height_lg: number = 22;
 export const base_font_family: string = `-apple-system, BlinkMacSystemFont, 'Helvetica Neue',
 Helvetica, Segoe UI, Arial, Roboto, 'PingFang SC', 'miui', 'Hiragino Sans GB',
 'Microsoft Yahei', sans-serif`;
@@ -50,26 +50,26 @@ export const price_integer_font_family: string = `Avenir-Heavy, PingFang SC, Hel
 sans-serif`;
 
 // Animation
-export const animation_duration_base = 300;
-export const animation_duration_fast = 200;
+export const animation_duration_base: number = 300;
+export const animation_duration_fast: number = 200;
 
 // Border
 export const border_color = gray_3;
-export const border_width_base = 1;
-export const border_radius_sm = 2;
-export const border_radius_md = 4;
-export const border_radius_lg = 8;
-export const border_radius_max = 999;
+export const border_width_base: number = 1;
+export const border_radius_sm: number = 2;
+export const border_radius_md: number = 4;
+export const border_radius_lg: number = 8;
+export const border_radius_max: number = 999;
 
 // Button
-export const button_mini_height = 24;
+export const button_mini_height: number = 24;
 export const button_mini_font_size = font_size_xs;
-export const button_small_height = 32;
+export const button_small_height: number = 32;
 export const button_small_font_size = font_size_sm;
 export const button_normal_font_size = font_size_md;
-export const button_large_height = 50;
-export const button_default_height = 44;
-export const button_default_line_height = 1.2;
+export const button_large_height: number = 50;
+export const button_default_height: number = 44;
+export const button_default_line_height: number = 1.2;
 export const button_default_font_size = font_size_lg;
 export const button_default_color = text_color;
 export const button_default_background_color = white;
@@ -95,8 +95,8 @@ export const button_disabled_opacity = disabled_opacity;
 
 // Cell
 export const cell_font_size = font_size_md;
-export const cell_line_height = 24;
-export const cell_vertical_padding = 10;
+export const cell_line_height: number = 24;
+export const cell_vertical_padding: number = 10;
 export const cell_horizontal_padding = padding_md;
 export const cell_text_color = text_color;
 export const cell_background_color = white;
@@ -108,7 +108,7 @@ export const cell_label_font_size = font_size_sm;
 export const cell_label_line_height = line_height_sm;
 export const cell_label_margin_top = padding_base;
 export const cell_value_color = gray_6;
-export const cell_icon_size = 16;
+export const cell_icon_size: number = 16;
 export const cell_right_icon_color = gray_6;
 export const cell_large_vertical_padding = padding_sm;
 export const cell_large_title_font_size = font_size_lg;
@@ -121,14 +121,14 @@ export const cell_group_title_padding_horizontal = padding_md;
 export const cell_group_title_padding_top = padding_md;
 export const cell_group_title_padding_bottom = padding_xs;
 export const cell_group_title_font_size = font_size_md;
-export const cell_group_title_line_height = 16;
+export const cell_group_title_line_height: number = 16;
 
 // Divider
 export const divider_vertical_margin = padding_md;
 export const divider_horizontal_margin = padding_md;
 export const divider_text_color = gray_6;
 export const divider_font_size = font_size_md;
-export const divider_line_height = 24;
+export const divider_line_height: number = 24;
 export const divider_border_color = border_color;
 export const divider_content_padding = padding_md;
 export const divider_content_left_width: string = '10%';
@@ -138,51 +138,51 @@ export const divider_content_right_width: string = '10%';
 export const loading_text_color = gray_6;
 export const loading_text_font_size = font_size_md;
 export const loading_spinner_color = gray_5;
-export const loading_spinner_size = 30;
-export const loading_spinner_animation_duration = 0.8;
+export const loading_spinner_size: number = 30;
+export const loading_spinner_animation_duration: number = 0.8;
 
 // NavBar
-export const nav_bar_height = 46;
+export const nav_bar_height: number = 46;
 export const nav_bar_background_color = white;
-export const nav_bar_arrow_size = 16;
+export const nav_bar_arrow_size: number = 16;
 export const nav_bar_icon_color = blue;
 export const nav_bar_text_color = blue;
 export const nav_bar_title_font_size = font_size_lg;
 export const nav_bar_title_text_color = text_color;
-export const nav_bar_z_index = 1;
+export const nav_bar_z_index: number = 1;
 
 // Image
-export const image_default_size = 100; // 图片默认尺寸
+export const image_default_size: number = 100; // 图片默认尺寸
 export const image_placeholder_text_color = gray_6;
 export const image_placeholder_font_size = font_size_md;
 export const image_placeholder_background_color = background_color;
-export const image_loading_icon_size = 32;
+export const image_loading_icon_size: number = 32;
 export const image_loading_icon_color = gray_4;
-export const image_error_icon_size = 32;
+export const image_error_icon_size: number = 32;
 export const image_error_icon_color = gray_4;
 
 // Overlay
-export const overlay_z_index = 1;
+export const overlay_z_index: number = 1;
 export const overlay_background_color: string = 'rgba(0, 0, 0, 0.7)';
 
 // Popup
 export const popup_background_color = white;
 // export const popup_transition: transform @animation-duration-base;
-export const popup_round_border_radius = 16;
-export const popup_close_icon_size = 22;
+export const popup_round_border_radius: number = 16;
+export const popup_close_icon_size: number = 22;
 export const popup_close_icon_color = gray_5;
 export const popup_close_icon_active_color = gray_6;
-export const popup_close_icon_margin = 16;
-export const popup_close_icon_z_index = 1;
+export const popup_close_icon_margin: number = 16;
+export const popup_close_icon_z_index: number = 1;
 
 // Tag
 export const tag_horizontal_padding = padding_base;
 export const tag_text_color = white;
 export const tag_font_size = font_size_sm;
-export const tag_border_radius = 2;
-export const tag_line_height = 16;
-export const tag_medium_vertical_padding = 2;
-export const tag_medium_horizontal_padding = 6;
+export const tag_border_radius: number = 2;
+export const tag_line_height: number = 16;
+export const tag_medium_vertical_padding: number = 2;
+export const tag_medium_horizontal_padding: number = 6;
 export const tag_large_vertical_padding = padding_base;
 export const tag_large_horizontal_padding = padding_xs;
 export const tag_large_border_radius = border_radius_md;
